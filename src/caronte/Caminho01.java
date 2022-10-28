98c0a5478aa48a842410e55fd8d9642cee3e1c20
Leonardo Silva <ldss1@outlook.com>
2022-10-27 21:12:49 -0300

    Projeto

---
+++ src/caronte/Caminho01.java
@@ -0,0 +1,10 @@
+package caronte;
+
+public class Caminho01 {
+
+	public int iniciar() {
+		return 0;
+	}
+
+	
+}
---
+++ src/caronte/Caminho02.java
@@ -0,0 +1,7 @@
+package caronte;
+
+public class Caminho02 {
+	public int iniciar() {
+		return 0;
+	}
+}
