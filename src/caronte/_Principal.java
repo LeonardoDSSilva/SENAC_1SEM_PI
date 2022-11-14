1fab2db9ceaaeddad962b2030e3486481da3c3ab
Leonardo Silva <ldss1@outlook.com>
2022-11-14 10:22:21 -0300

    classe principal

--- src/caronte/_Principal.java
+++ src/caronte/_Principal.java
@@ -10,14 +10,13 @@ public class _Principal {
 		//sysout
 
 		// Test
-		int caminho, pontos = 0;
 		boolean continuar = true;
 		Scanner sc_Num = new Scanner(System.in);
 		Scanner sc_Str = new Scanner(System.in);
 
 		limparTela();
 
-		temporizador(5);
+		temporizador(1);
 
 		System.out.println("Caronte - O Barqueiro do Inferno");
 		System.out.println("================================");
@@ -25,35 +24,57 @@ public class _Principal {
 		System.out.println("Seja bem-vindo ao jogo Caronte!");
 		System.out.println("Você está prestes a embarcar em uma jornada pelo Inferno.");
 
+		boolean caminhoValido = false;
+		int caminho = 0, pontos = 0;
+		int[] caminhos = new int[4];
+		int i = 0;
+
 		do {
+			limparTela();
 			System.out.println("Escolha o caminho que deseja seguir:");
 
-			
-			System.out.println("1 - Caminho 01");
-			System.out.println("2 - Caminho 02");
-			System.out.println("3 - Caminho 03");
-			System.out.println("4 - Caminho 04");
-			System.out.println("5 - Sair");
-		
-			System.out.println("O caminho: ");
-			caminho = sc_Num.nextInt();
-			
-			menuOCaminho(caminho);
-			
-			limparTela();
-			System.out.print("O caminho: ");
-			caminho = sc_Num.nextInt();
+			int cont = 1;
+			for (int j = 0; j < caminhos.length ; j++) {
+				if (cont == caminhos[j]) {
+					System.out.println(" " + cont  + " - já foi escolhido");
+				}
+				else{
+					System.out.println(" " + cont + " - disponível");
+				}
+				cont ++;
+			}
 			
 			temporizador(2);
-			
-			// Array 
 
+			System.out.println("O caminho: ");
+			caminho = sc_Num.nextInt();
+
+			if (i == 5){
+				caminho = 5;
+			}
+			else if (caminho > 0 && caminho < 5) {
+
+				for (int index = 0; index < i; index++) {
+					if (caminho == caminhos[index]) {
+						caminhoValido = true;
+					}
+				}
+				if (!caminhoValido) {
+					caminhos[i] = caminho;
+					i++;
+					menuOCaminho(caminho);
+				}
+				else{
+					// Exibir todos os caminhos já escolhidos
+					System.out.println("Escolha outro caminho");
+				}
+			}
 			// Criar uma condição para sair do loop
-			
-		} while (continuar);
+		} while (caminho != 5);
 		
-			limparTela();
-			caminho05(pontos);
+		System.out.println("Escolha out4o caminho");
+		// 	limparTela();
+		// 	caminho05(pontos);
 	}
 
 	public static void limparTela() {
@@ -65,7 +86,6 @@ public class _Principal {
 		try {
 			Thread.sleep(tempoEmSegundos * 1000);
 		} catch (InterruptedException e) {
-			
 			e.printStackTrace();
 		}
 	}
@@ -97,7 +117,6 @@ public class _Principal {
 	}
 
 	public static void caminho05(int pontos) {
-		
 		System.out.println("Você chegou ao fim do jogo!");
 		System.out.println("Você conseguiu " + pontos + " pontos!");
 	}
