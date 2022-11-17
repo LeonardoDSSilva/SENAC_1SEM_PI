5673d8ebb3d2fed78ea97026b51bb77219b7ba18
Pipoca <rmr.eu@hotmail.com.br>
2022-11-17 15:25:55 -0300

    Caminho 2 update

--- src/caronte/Caminho03.java
+++ src/caronte/Caminho03.java
@@ -1,8 +1,30 @@
 package caronte;
 
+import java.util.Scanner;
+
 public class Caminho03 {
 
 	public int iniciar() {
+		//JOGO DE PAR OU IMPAR
+		Scanner lenin = new Scanner(System.in);
+		Boolean margin = false;
+		int escolhaM = 0;
+		System.out.println(" '-Voce navega pelas aguas calmas do rio, em uma das margens, voce avista uma mulher. Determinada olhando para voce.-' ");
+		System.out.println("=====IR ATÉ A MARGEM?===== \n[1] - SIM                  [2] - NÃO");
+		escolhaM = lenin.nextInt();
+
+		if(escolhaM == 1){
+			margin =true;
+			}else if(escolhaM == 2){
+				System.out.println("diga tchau");
+				}else if(escolhaM > 2){
+					System.out.println("opção invalida!");
+					}
+
+
+
+
+
 		return 0;
 	}
 	
